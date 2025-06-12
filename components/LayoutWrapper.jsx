da5d10e27a7d043667d9187f7b9c51a3eeb3c25a
Oraclesid1411 <giloracle76@gmail.com>
2025-06-12 09:49:00 +0000

    Fix: marquage client pour AuthenticateContext

---
+++ components/LayoutWrapper.jsx
@@ -0,0 +1,26 @@
+// app/components/LayoutWrapper.jsx
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Header_menu from '../components/Header_menu';
+import FixedMenu from '../components/FixedMenu';
+import { Suspense } from 'react';
+import { ToastContainer } from 'react-toastify';
+
+export default function LayoutWrapper({ children }) {
+  const pathname = usePathname();
+  const isAuthPage = ['/login', '/register', '/forgotpass'].includes(pathname);
+
+  return (
+    <div id="root">
+      {!isAuthPage && <Header_menu />}
+      <main>
+        <ToastContainer className="toast_style" />
+        <Suspense fallback={<div>Chargement...</div>}>
+          {children}
+        </Suspense>
+      </main>
+      {!isAuthPage && <FixedMenu />}
+    </div>
+  );
+}
